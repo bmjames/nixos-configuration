2afe85fae2f316adefc460fbad3742f1faa8e28e
Ben James <bmjames@gmail.com>
2015-05-24 22:01:38 +0000

    Enable xfce

--- configuration.nix
+++ configuration.nix
@@ -73,8 +73,9 @@ in
   services.xserver.xkbOptions = "eurosign:e";
 
   # Enable the KDE Desktop Environment.
-  services.xserver.displayManager.kdm.enable = true;
-  services.xserver.desktopManager.kde4.enable = true;
+  # services.xserver.displayManager.kdm.enable = true;
+  # services.xserver.desktopManager.kde4.enable = true;
+  services.xserver.desktopManager.xfce.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.extraUsers.ben = {
